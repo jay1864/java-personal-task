a496cef6013a674308f3e32e7a445a37f8aaab96
vieve <171054334+jay1864@users.noreply.github.com>
2024-07-29 12:31:54 +0900

    feat: Level1-4
    반복문을 사용하여 반복의 종료를 알려주는 “exit” 문자열을 입력하기 전까지 무한으로 계산을 진행할 수 있도록 소스 코드를 수정합니다.

--- src/calculator/App.java
+++ src/calculator/App.java
@@ -6,26 +6,30 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        int num1 = sc.nextInt();
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        int num2 = sc.nextInt();
+        do{
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int num1 = sc.nextInt();
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int num2 = sc.nextInt();
 
-        System.out.print("사칙연산 기호를 입력하세요: ");
-        char operator = sc.next().charAt(0);
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            char operator = sc.next().charAt(0);
 
-        double result = 0;
-        switch (String.valueOf(operator)){
-            case "+" -> result = num1 + num2;
-            case "-" -> result = num1 - num2;
-            case "*" -> result = num1 * num2;
-            case "/" -> {
-                if (num2 == 0) System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                else result = (double)num1 / num2;
-            }
-            default -> System.out.println("연산기호를 확인해 주십시오.");
-        };
-        System.out.println("결과: " + String.format("%.1f", result));
+            double result = 0;
+            switch (String.valueOf(operator)){
+                case "+" -> result = num1 + num2;
+                case "-" -> result = num1 - num2;
+                case "*" -> result = num1 * num2;
+                case "/" -> {
+                    if (num2 == 0) System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                    else result = (double)num1 / num2;
+                }
+                default -> System.out.println("연산기호를 확인해 주십시오.");
+            };
+            System.out.println("결과: " + String.format("%.1f", result));
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+        }while(!sc.next().equals("exit"));
+
 
     }
 }
