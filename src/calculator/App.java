94758ba71ceb2a2857cb4632975fac02cbd9b866
vieve <171054334+jay1864@users.noreply.github.com>
2024-07-29 12:39:41 +0900

    feat: Level1-5
    연산 결과 10개를 저장할 수 있는 배열을 선언 및 생성하고 연산의 결과를 저장합니다.

--- src/calculator/App.java
+++ src/calculator/App.java
@@ -6,12 +6,14 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        double[] resultList = new double[10];
+        int index = 0;
+
         do{
             System.out.print("첫 번째 숫자를 입력하세요: ");
             int num1 = sc.nextInt();
             System.out.print("두 번째 숫자를 입력하세요: ");
             int num2 = sc.nextInt();
-
             System.out.print("사칙연산 기호를 입력하세요: ");
             char operator = sc.next().charAt(0);
 
@@ -25,9 +27,11 @@ public class App {
                     else result = (double)num1 / num2;
                 }
                 default -> System.out.println("연산기호를 확인해 주십시오.");
-            };
+            }
+            resultList[index++] = result;
             System.out.println("결과: " + String.format("%.1f", result));
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+
         }while(!sc.next().equals("exit"));
 
 
